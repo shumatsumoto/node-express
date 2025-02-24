ba274a9979f517043d9b6b1692885ca637d2ccff
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-02-24 22:54:11 +0900

    Getリクエスト：文字列を返す

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
 app.listen(5050, () => {
-    console.log('Server is running on http://localhost:5050');
+  console.log('Server is running on http://localhost:5050');
 });
