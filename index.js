a91a0f46c06a75fab9b8700dcc92e6c9c124d634
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-03-02 23:24:17 +0900

    getリクエスト

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const coronaData = require('./coronaData');
 
 app.get("/", (req, res) => {
-  res.send(coronaData);
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(5050, () => {
