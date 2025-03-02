11215fb67e9f1d00b146f63baffd5dbf81c69408
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-03-02 23:51:36 +0900

    Putリクエスト

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
+const activities = require("./activities");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -11,6 +12,11 @@ app.post("/autumn", (req, res) => {
   fs.writeFile(__dirname + "/data.txt", req.body.activity, () => {
     res.send("Data saved to file");
   })
+})
+
+app.get("/update", (req, res) => {
+  console.log(activities);
+  res.send(activities);
 })
 
 app.listen(5050, () => {
