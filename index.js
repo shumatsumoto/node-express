560aee9bc771def03a2c2e7ba3fadab6969cc3cb
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-03-02 23:33:53 +0900

    postリクエスト実装

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/autumn", (req, res) => {
+  console.log(req.body.activity);
+})
+
 
 app.listen(5050, () => {
   console.log('Server is running on http://localhost:5050');
